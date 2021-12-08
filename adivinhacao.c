eca36b8a952e6a4936399316306e7196417991b8
Marco Antonio da Silva Francisco <marcospatton@hotmail.com>
2021-12-08 01:01:45 -0300

    Atualização jogo adivinha
    Atualização jogo adivinha

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,7 +1,20 @@
 #include<stdio.h>
 
 int main(){
+
+	//Imprime cabecalho do nosso jogo
 	printf("***************************************\n");
-	printf("Bem vindo ao nosso jogo de adivinhação \n");
+	printf("Bem vindo ao nosso jogo de adivinhacao \n");
 	printf("***************************************\n");
+
+	int numerosecreto = 42;
+
+	int chute;
+
+	printf("Qual e o seu chute ? ");
+	scanf("%d", &chute);
+	printf("Seu chute foi %d", chute);
+
+	printf("%d %d\n ", numerosecreto);
+
 }	
