b9e964a0a7facbd5df6e6f5f7c59178b95bdaa0a
Marco Antonio da Silva Francisco <marcospatton@hotmail.com>
2021-12-08 00:42:23 -0300

    Add files via upload

---
+++ adivinhacao.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+	printf("***************************************\n");
+	printf("Bem vindo ao nosso jogo de adivinhação \n");
+	printf("***************************************\n");
+}	
