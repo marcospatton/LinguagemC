c1be1d8e85c2f6f62d54d98ebddd9dadeca4ec69
Marco Antonio da Silva Francisco <marcospatton@hotmail.com>
2021-12-09 11:08:00 -0300

    Atualizado adivinhação
    Atualizado adivinhação

--- adivinhacao.c
+++ adivinhacao.c
@@ -10,25 +10,43 @@ int main(){
 	int numerosecreto = 42;
 
 	int chute;
+	int tentativas = 1;
+	
+	while(1){
 
+	printf("Tentativa %d\n", tentativas);
 	printf("Qual e o seu chute ? ");
+
+
 	scanf("%d", &chute);
 	printf("Seu chute foi %d\n", chute);
 
-	if(chute == numerosecreto){
+	if(chute < 0){
+		printf("Voce nao pode digitar numeros negativos!\n");		
+		continue;
+	}
+
+
+	int acertou = (chute == numerosecreto);
+	int maior = chute > numerosecreto;
+
+	if(acertou){
 		printf("Parabens! Voce acertou!\n");
 		printf("Jogue de novo, voce e um bom jogador!\n");
-	}
-	else{
 
-		if(chute > numerosecreto){
+		break;
+	}	
+		else if(maior){
 			printf("Seu chute foi maior que o numero secreto\n");
 		}
 
-			if(chute < numerosecreto){
-				printf("Seu chute foi menor que o numerosecreto\n");
-			}
-			printf("Voce errou!\n");
-			printf("Mas nao desanime, tente de novo!\n");
-	}
-}	
+		else {
+			printf("Seu chute foi menor que o numero secreto\n");
+		}
+		tentativas = tentativas + 1;
+	
+	}	
+
+printf("Fim de jogo!\n");
+printf("Voce acertou em %d tentativas!", tentativas -1);
+}
